eabac583ea002effa6361b5f29dc442feef9345a
SWADHIN SATYAPRAKASH MAJHI <95016250+swadhin1237@users.noreply.github.com>
2023-04-04 15:34:39 +0530

    test completed

--- FRSS/shop/tests/test_urls.py
+++ FRSS/shop/tests/test_urls.py
@@ -9,12 +9,76 @@ class TestUrls(TestCase):
         url = reverse('AboutUs',args=[3])
         self.assertEquals(resolve(url).func,about)
 
-    def test_about_us_resolved(self):
+    def test_productView_resolved(self):
         url = reverse('ProductView', args=[3,4])
         self.assertEquals(resolve(url).func, productView)
 
+    def test_shophome_resolved(self):
+        url = reverse('ShopHome',args=[3])
+        self.assertEquals(resolve(url).func,index)
 
+    def test_contact_resolved(self):
+        url = reverse('ContactUs', args=[3])
+        self.assertEquals(resolve(url).func, contact)
 
+    def test_checkout_resolved(self):
+        url = reverse('Checkout', args=[3])
+        self.assertEquals(resolve(url).func, checkout)
 
+    def test_learn_resolved(self):
+        url = reverse('Learn')
+        self.assertEquals(resolve(url).func, learn)
 
+    def test_signup_resolved(self):
+        url = reverse('signup')
+        self.assertEquals(resolve(url).func, signup)
 
+    def test_login_resolved(self):
+        url = reverse('log_in')
+        self.assertEquals(resolve(url).func, log_in)
+################################################
+
+    def test_history_resolved(self):
+        url = reverse('history', args=[3])
+        self.assertEquals(resolve(url).func, history)
+
+    def test_explore_resolved(self):
+        url = reverse('explore')
+        self.assertEquals(resolve(url).func, explore)
+
+    def test_profile_resolved(self):
+        url = reverse('profile', args=[3])
+        self.assertEquals(resolve(url).func, profile)
+
+    def test_logout_resolved(self):
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func, log_out)
+
+    def test_checkout_resolved(self):
+        url = reverse('Checkout', args=[3])
+        self.assertEquals(resolve(url).func, checkout)
+
+    def test_editProfile_resolved(self):
+        url = reverse('editProfile',args=[3])
+        self.assertEquals(resolve(url).func, editProfile)
+
+    def test_editProfile2_resolved(self):
+        url = reverse('editProfile2', args=[3])
+        self.assertEquals(resolve(url).func, editProfile2)
+
+    def test_payment_resolved(self):
+        url = reverse('payment',args=[3])
+        self.assertEquals(resolve(url).func, payment)
+
+    def test_change_password_resolved(self):
+        url = reverse('change_password',args=[3])
+        self.assertEquals(resolve(url).func, change_password)
+
+    def test_payment2_resolved(self):
+        url = reverse('payment2', args=[3])
+        self.assertEquals(resolve(url).func, payment2)
+
+    def test_review_resolved(self):
+        url = reverse('review', args=[3,5])
+        self.assertEquals(resolve(url).func, review)
+#############################################
--- FRSS/shop/tests/test_views.py
+++ FRSS/shop/tests/test_views.py
@@ -8,9 +8,16 @@ class TestViews(TestCase):
         self.client = Client()
         self.test_user = User.objects.create(username="avenger")
         self.test_user.set_password("venky")
-
-
-
+        self.test_review = Review.objects.create(user_id="1", product_id="1", user_review="This is nice product", user_name="avenger")
+        self.test_cust = Customer.objects.create(user=self.test_user, first_name="avenger", last_name="assemble", email="venk@gmail.com",phone="90897")
+        self.test_prod = Product.objects.create(product_name="bed" ,category="Furniture",
+                                                sub_category="bed",
+                                                price="12344",
+                                                currentNumber="23",
+                                                totalNumber="123",
+                                                product_desc="A nice bed",
+                                                pub_date="2005-12-20",
+                                                color="brown")
     def test_index(self):
         response = self.client.get(reverse('ShopHome',args=[self.test_user.id]))
 
@@ -23,3 +30,116 @@ class TestViews(TestCase):
 
         self.assertEquals(response.status_code,200)
         self.assertTemplateUsed(response, 'shop/change_password.html')
+
+    def test_review(self):
+        response = self.client.post(reverse('review', args=[self.test_prod.id,self.test_cust.user.id]), {'cust_review': str(self.test_review.user_review)})
+        self.assertEquals(response.status_code, 302)
+
+    def test_payment(self):
+        response = self.client.post(reverse('payment', args=[self.test_cust.user.id]), {'credit_card_number':"123",
+                                                                                        'month':"feb",
+                                                                                        'year':"2022",
+                                                                                        'cvv':"212",
+                                                                                        'payment':"23400"})
+        self.assertEquals(response.status_code, 302)
+    def test_payment2(self):
+        response = self.client.get(reverse('payment2', args=[self.test_cust.user.id]))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response,'shop/payment.html')
+
+    def test_editProfile2(self):
+        response = self.client.get(reverse('editProfile2', args=[self.test_cust.user.id]))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response,'shop/editprofile.html')
+
+    def test_editProfile(self):
+        response = self.client.post(reverse('editProfile', args=[self.test_cust.user.id]), {'first_name':"we" ,'last_name':"rt",'email':"sd@gmail.com",'phone':"1234"})
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response,'shop/editprofile.html')
+
+    def test_about(self):
+        response = self.client.get(
+            reverse('AboutUs', args=[self.test_user.id]))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/about.html')
+
+    def test_contact(self):
+        response = self.client.get(
+            reverse('ContactUs', args=[self.test_user.id]))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/contact.html')
+
+    def test_learn(self):
+        response = self.client.get(reverse('Learn'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/learn.html')
+
+    def test_tracker(self):
+        response = self.client.get(reverse('Tracker'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/tracker.html')
+
+    def test_log_in(self):
+        response = self.client.post(
+            reverse('log_in'), {'username': 'avenger', 'password': 'venky'})
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/login.html')
+
+    def test_signup(self):
+        response = self.client.post(reverse('signup'), data={
+            'username': 'swadhinbitch',
+            'first_name': 'swadhin',
+            'last_name': 'majhi',
+            'email': 'kyapata',
+            'phone': '69',
+            'password': 'kyapata',
+            'confirm_password': 'kyapata'})
+
+        self.assertEquals(response.status_code, 302)
+        users = User.objects.all()
+        self.assertEqual(users.count(), 2)
+
+    def test_product_view(self):
+        response = self.client.get(
+            reverse('ProductView', args=[self.test_prod.id, self.test_cust.user.id]))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/prodView.html')
+
+    def test_checkout(self):
+        response = self.client.get(
+            reverse('Checkout', args=[self.test_cust.user.id]))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/checkout.html')
+
+    def test_histpry(self):
+        response = self.client.get(
+            reverse('history', args=[self.test_cust.user.id]))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/history.html')
+
+    def test_explore(self):
+        response = self.client.get(reverse('explore'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/demoview.html')
+
+    def test_profile(self):
+        response = self.client.get(
+            reverse('profile', args=[self.test_cust.user.id]))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/profile.html')
+
+    def test_logout(self):
+        response = self.client.get(reverse('logout'))
+
+        self.assertEquals(response.status_code, 302)
+        # self.assertTemplateUsed(response,'shop/checkout.html')
